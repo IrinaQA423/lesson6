a363825a20a0507e890b7eec160a7940a75ed491
IrinaQA423 <irina-koshina@mail.ru>
2025-04-09 20:21:39 +0800

    Update password.py

--- password.py
+++ password.py
@@ -1,17 +1,22 @@
 def is_very_long(password):
     return len(password) > 12
 
+
 def has_digit(password):
     return any(i.isdigit()for i in password)
+
 
 def has_upper_letters(password):
     return any(i.isupper()for i in password)
 
+
 def has_lower_letters(password):
     return any(i.islower()for i in password)
 
+
 def has_symbols(password):
-     return any(not i.isdigit() and not i.isalpha()for i in password)
+    return any(not i.isdigit() and not i.isalpha()for i in password)
+
 
 def main():
     password = input("Введите  пароль: ")
