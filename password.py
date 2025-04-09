c0875937547134d982bc3cf66b0d291057d184ab
IrinaQA423 <irina-koshina@mail.ru>
2025-04-09 20:18:45 +0800

    исправления3

--- password.py
+++ password.py
@@ -1,19 +1,19 @@
+def is_very_long(password):
+    return len(password) > 12
+
+def has_digit(password):
+    return any(i.isdigit()for i in password)
+
+def has_upper_letters(password):
+    return any(i.isupper()for i in password)
+
+def has_lower_letters(password):
+    return any(i.islower()for i in password)
+
+def has_symbols(password):
+     return any(not i.isdigit() and not i.isalpha()for i in password)
+
 def main():
-    def is_very_long(password):
-        return len(password) > 12
-
-    def has_digit(password):
-        return any(i.isdigit()for i in password)
-
-    def has_upper_letters(password):
-        return any(i.isupper()for i in password)
-
-    def has_lower_letters(password):
-        return any(i.islower()for i in password)
-
-    def has_symbols(password):
-        return any(not i.isdigit() and not i.isalpha()for i in password)
-
     password = input("Введите  пароль: ")
 
     score = 0
@@ -28,7 +28,7 @@ def main():
     for foo in list_foo:
         if foo(password):
             score += 2
-            print("Рейтинг пароля: ", score)
+    print("Рейтинг пароля: ", score)
 
 
 if __name__ == '__main__':
